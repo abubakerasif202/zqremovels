cc89b79f0e6e23eca3955e8480d120c5936148b1
Abubakar Asif <Abubakarasif202@hotmail.com>
2026-06-12 15:24:49 +1000

    fix: update astro config for tailwind vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static',
   outDir: './site-dist',
-  build: {
-    format: 'preserve'
-  },
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
